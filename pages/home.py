1137553e0ef50ed0dcee519c8697e8030a3c5cdb
rumanimaseera <rumanimaseera123@gmail.com>
2025-04-21 12:21:19 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -107,7 +107,7 @@ if "current_section" not in st.session_state:
 def navigate_to(section):
     if section == "Login":
         # Switch to the authentication page (no '.py' extension needed)
-        st.switch_page("authentication")  # Ensure this matches the page name without '.py'
+        st.switch_page("pages/authentication.py")  # Ensure this matches the page name without '.py'
     elif section:
         # Store the current section in session state for navigation purposes
         st.session_state.current_section = section
